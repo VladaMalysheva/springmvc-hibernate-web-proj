e32ca31477cc4b8ffa797dc251486e45cabb8a20
VladaMalysheva <ik02.vlada.m@gmail.com>
2023-01-15 20:37:30 +0300

    Added rest getCustomer and getCustomers

---
+++ src/main/java/com/example/webproject/rest/CustomerController.java
@@ -0,0 +1,30 @@
+package com.example.webproject.rest;
+
+import com.example.webproject.entity.Customer;
+import com.example.webproject.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService service;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers(){
+
+        return service.getCustomers();
+    }
+
+    @GetMapping("/customers/{id}")
+    public Customer getCustomer(@PathVariable int id){
+        return service.getCustomer(id);
+    }
+}
